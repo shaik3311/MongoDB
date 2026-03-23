24c417cdfc378d254577f5596be6af4f0b53955c
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-03-23 09:30:07 +0530

    dataFinding

---
+++ 02_datafinding.mongodb.js
@@ -0,0 +1,14 @@
+use("ecomerce");
+
+
+// Find all documents in a collection without any filters 
+// db.products.find();  
+
+// Find products filtering with category 
+// db.products.find({"category":"Electronics"});
+
+// Find products based on their prices using mongoDB operators 
+// db.products.find({"price":{$gt:2000}});
+
+// Find produts in range 
+// db.products.find({"price":{$gte:1000,$lte:3000}})
