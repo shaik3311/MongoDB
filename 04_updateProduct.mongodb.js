b1ada59f65b59b91437a1eb228f20d881c3f40a4
shaik3311 <162719241+shaik3311@users.noreply.github.com>
2026-03-23 09:59:56 +0530

    update Data

---
+++ 04_updateProduct.mongodb.js
@@ -0,0 +1,7 @@
+use("ecomerce")
+
+// Update a documents price 
+// db.products.update({"name":"Wireless Mouse"},{$set:{"price":599}})
+
+// Increase all products prices by 10 %
+// db.products.updateMany({},{$mul:{"price":1.1}})
